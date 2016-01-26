40d9b27e57d9efc15603d1c8310745e8e7b3c4cf
ayao <ayao@example.net>
2016-01-26 10:26:25 +0800

    1.refactor webpack 2.add bootstrap

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@
 'use strict';
 
 import React from 'react';
-import Hello from './component.jsx';
+import Hello from './component.js';
 import './main.css';
 
 main();
--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,18 @@ var path = require('path');
 var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
+var APP_DIR = path.resolve(__dirname, 'app');
+var BUILD_DIR = path.resolve(__dirname, 'build');
+
 config = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+    entry: ['webpack/hot/dev-server', APP_DIR + '/main.js'],
     resolve: {
         alias: {
             'react': pathToReact
         }
     },
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
