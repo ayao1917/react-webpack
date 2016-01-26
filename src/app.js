1464b2f10445c238d85ecb3e70dd3e05bf4e22e8
ayao <ayao@example.net>
2016-01-26 16:27:32 +0800

    add react-router

--- src/app.js
+++ src/app.js
@@ -4,11 +4,32 @@
 'use strict';
 
 import React from 'react';
-import Hello from './js/component.js';
+import ReactDOM from 'react-dom';
+import Router from 'react-router';
+import { DefaultRoute, Link, Route, RouteHandler} from 'react-router';
+
+import LoginHandler from './js/login.js';
 import './css/main.css';
 
-app();
+let App = React.createClass({
+    render() {
+        return (
+            <ul className="nav">
+                <li><Link to="app">Home</Link></li>
+                <li><Link to="login">Login</Link></li>
 
-function app() {
-    React.render(<Hello/>, document.getElementById('app'));
-}
+                <RouteHandler/>
+            </ul>
+        );
+    }
+});
+
+let routes = (
+    <Route name="app" path="/" handler={App}>
+        <Route name="login" path="/login" handler={LoginHandler}/>
+    </Route>
+);
+
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.body);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,12 @@
  */
 var path = require('path');
 var node_modules = path.resolve(__dirname, 'node_modules');
-var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 var APP_DIR = path.resolve(__dirname, 'src');
 var BUILD_DIR = path.resolve(__dirname, 'build');
 
 config = {
     entry: ['webpack/hot/dev-server', APP_DIR + '/app.js'],
-    resolve: {
-        alias: {
-            'react': pathToReact
-        }
-    },
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
@@ -30,8 +24,7 @@ config = {
         }, {
             test: /\.css$/, // Only .css files
             loader: 'style!css' // Run both loaders
-        }],
-        noParse: [pathToReact]
+        }]
     }
 };
 
